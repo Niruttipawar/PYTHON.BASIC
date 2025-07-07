2c22c7efa80b8b1260c509681768dff1f705e3f3
Nivrutti pawar <159421324+Niruttipawar@users.noreply.github.com>
2025-07-07 13:44:27 +0530

    Add files via upload
    My second program is this one. It is a simple calculator.

---
+++ add.py
@@ -0,0 +1,14 @@
+a=int(input("Enter first number: "))  # Input for first number
+
+b=int(input("Enter second number: "))  # Input for second number
+
+print("The addition of numbers is:", a + b)
+
+print("The subtraction of numbers is:", a - b)  
+
+print("The multiplication of numbers is:", a * b)
+
+print("The division of numbers is:", a / b)  # Division operation
+
+print("The modulus of numbers is:", a % b)  # Modulus operation
+# This code performs basic arithmetic operations on two integers
